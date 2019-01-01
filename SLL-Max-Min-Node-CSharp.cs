c92e9f16811109aaf3e372795de76d7dca199863
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2019-01-01 21:19:06 +0500

    Add files via upload

---
+++ SLL-Max-Min-Node-CSharp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SLL_Max_Min_Node_CSharp
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+    public class LinkedList
+    {
+        private Node start, temp, current;
+        public LinkedList() => start = temp = current = null;
+        // Insertion in SLL (Add Last)
+        public void AddLast(int value)
+        {
+            // Create Node ...
+            temp = new Node
+            {
+                Data = value,
+                Next = null
+            };
+
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+
+            // Go to end of linked-list ...
+            current = start;
+            while (current.Next != null)
+                current = current.Next;
+
+            current.Next = temp;
+        }
+        // Find Max value
+        public int Max()
+        {
+            current = start;
+            int max = current.Data;
+            while(current != null)
+            {
+                if (current.Data > max)
+                    max = current.Data;
+                current = current.Next;
+            }
+            return max;
+        }
+        // Find Min value
+        public int Min()
+        {
+            current = start;
+            int min = current.Data;
+            while (current != null)
+            {
+                if (current.Data < min)
+                    min = current.Data;
+                current = current.Next;
+            }
+            return min;
+        }
+    }
+}
---
+++ SLL-SumOfNodes-CSharp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SLL_SumOfNodes_CSharp
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+    public class LinkedList
+    {
+        private Node start, temp, current;
+        public LinkedList() => start = temp = current = null;
+        // Insertion in SLL (Add Last)
+        public void AddLast(int value)
+        {
+            // Create Node ...
+            temp = new Node
+            {
+                Data = value,
+                Next = null
+            };
+
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+
+            // Go to end of linked-list ...
+            current = start;
+            while (current.Next != null)
+                current = current.Next;
+
+            current.Next = temp;
+        }
+        // Traversing in SLL (Display Nodes)
+        public string DisplayList()
+        {
+            string list = "";
+            current = start;
+            while(current != null)
+            {
+                list += current.Data + " ";
+                current = current.Next;
+            }
+            return list;
+        }
+        // Traversing in SLL (Sum of Nodes)
+        public int SumOfNodes()
+        {
+            int sum = 0;
+            current = start;
+            while (current != null)
+            {
+                sum += current.Data;
+                current = current.Next;
+            }
+            return sum;
+        }
+    }
+}
