1238752e502f10eac3fea39b498c7fbf0be2fe42
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2019-01-02 21:16:54 +0500

    Add files via upload

---
+++ CircularSLL1-CSharp-Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace CSLL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            Write("Enter Total Number of Nodes : ");
+            int n = ToInt32(ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                WriteLine($"Enter Record {i + 1}");
+
+                Write("Enter Roll-No : ");
+                int rollNo = ToInt32(ReadLine());
+                Write("Enter Name : ");
+                string name = ReadLine();
+                Write("Enter Degree : ");
+                string degree = ReadLine();
+                Write("Enter CGPA : ");
+                double cgpa = ToDouble(ReadLine());
+
+                list.InsertNode(rollNo, name, degree, cgpa);
+            }
+
+            WriteLine("Display Records of Students");
+            WriteLine(list.DisplayList());
+
+            ReadKey(true);
+        }
+    }
+}
---
+++ CircularSLL1-CSharp.cs
@@ -0,0 +1,65 @@
+﻿// Circularly Linked-List
+
+using System;
+
+namespace CSLL1
+{
+    public class Node
+    {
+        public int roll_no;
+        public string name;
+        public string degree;
+        public double cgpa;
+        public Node link;
+    }
+    public class LinkedList
+    {
+        private Node start, temp, current;
+        public LinkedList()
+        {
+            start = temp = current = null;
+        }
+        // Insertion in CSLL (Insert Node)
+        public void InsertNode(int _rollNo, string _name, string _degree, double _cgpa)
+        {
+            // Create Node ...
+            temp = new Node();
+            temp.roll_no = _rollNo;
+            temp.name = _name;
+            temp.degree = _degree;
+            temp.cgpa = _cgpa;
+
+            if(start == null)
+            {
+                start = temp;
+                temp.link = start;  // * Assign the address of starting node in the pointer field of last node ...
+                return;
+            }
+
+            // Go to end of linked-list ...
+            current = start;
+            while (current.link != start)
+                current = current.link;
+
+            current.link = temp;
+            temp.link = start;      // * Assign the address of starting node in the pointer field of last node ...
+        }
+        // Traversing in CSLL (Display List)
+        public string DisplayList()
+        {
+            string list = "";
+            if (start == null)
+            {
+                list += "List is Empty!";
+                return list;
+            }
+            current = start;
+            do
+            {
+                list += $"RollNo: {current.roll_no} - Name: {current.name} - Degree: {current.degree} - CGPA: {current.cgpa}\n";
+                current = current.link;
+            } while (current != start);
+            return list;
+        }
+    }
+}
